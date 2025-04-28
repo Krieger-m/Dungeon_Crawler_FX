adbb63d87660389e399d1e8bf870f81559aaff22
Mat <156411293+Krieger-m@users.noreply.github.com>
2025-04-28 21:29:09 +0200

    upload_04
    updates and bug-fixes... still a mess but advances slowly but steady

--- src/main/java/com/krieger/dungeon_crawler_fx/App.java
+++ src/main/java/com/krieger/dungeon_crawler_fx/App.java
@@ -1,86 +1,17 @@
 package com.krieger.dungeon_crawler_fx;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import java.io.IOException;
-
-import static com.krieger.dungeon_crawler_fx.HelloController.*;
-
 
 public class App extends Application {
 
-    Insets inset = new Insets(20);
-    public String imgPath = "file:./images/start_scene.jpg";
-    public Image img = new Image(imgPath);
-    public ImageView imageView = new ImageView(img);
-
-
-    public StackPane root = new StackPane();
-    public VBox vBox = new VBox();
-    public HBox btnBox = new HBox();
-
-    public StackPane stack = new StackPane();
-
-    public static Button actionBtn = new Button("Action!");
-    public Button inventoryBtn = new Button("Inventory!");
-
-
     @Override
-    public void start(Stage stage) throws IOException {
-
-                // Root-background setting
-        root.setBackground(new Background(new BackgroundFill(Color.web("#4d4d4d"),null,null)));
-
-                // Button-Box settings
-        btnBox.setPadding(inset);
-        btnBox.setSpacing(20);
-
-                // image-settings
-        imageView.setFitHeight(800);
-
-        Text beginning = textAnimation();
-
-
-
-                // Assembly
-        btnBox.getChildren().addAll(actionBtn,inventoryBtn);
-
-        stack.getChildren().addAll(imageView,beginning);
-
-        vBox.getChildren().addAll(stack,btnBox);
-        root.setAlignment(Pos.CENTER);
-
-        root.getChildren().addAll(vBox);
-
-                // TODO Inventory stuff here...
-
-
-
-        actionBtn.setOnAction(actionEvent  -> {
-
-        });
-
-
-                // Scene-assembly
-        Scene scene = new Scene(root, 1024, 1000);
-
-
-                // Stage-assembly
-        stage.setTitle("Dungeon_Adventure+");
-        stage.setScene(scene);
+    public void start(Stage stage) {
+        // Set the intro scene
+        stage.setScene(IntroController.getIntroScene(stage));
+        stage.setTitle("Dungeon Adventure");
         stage.show();
     }
-
-
 
     public static void main(String[] args) {
         launch();
---
+++ src/main/java/com/krieger/dungeon_crawler_fx/ButtonController.java
@@ -0,0 +1,12 @@
+package com.krieger.dungeon_crawler_fx;
+
+import javafx.scene.control.Button;
+
+public class ButtonController {
+
+    // Button elements
+    public static Button actionBtn = new Button("Action!");
+    public Button inventoryBtn = new Button("Inventory!");
+
+
+}
