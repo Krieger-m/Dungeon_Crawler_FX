71be2e806374b9513f0a00313c54c7837ae6ecc0
Mat <156411293+Krieger-m@users.noreply.github.com>
2025-05-06 09:56:01 +0200

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.krieger.dungeon_crawler_fx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.krieger.dungeon_crawler_fx to javafx.fxml;
-    exports com.krieger.dungeon_crawler_fx;
-}
