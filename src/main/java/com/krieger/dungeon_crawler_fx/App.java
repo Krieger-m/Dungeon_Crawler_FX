7ad2d2e4bc7a358dc9d09a9b732871ae0d66bd8e
Mat <156411293+Krieger-m@users.noreply.github.com>
2025-05-07 12:10:57 +0200

    v_016
    some more progress.   
    currently trying to solve a problem with the button-eventHandlers

--- src/main/java/com/krieger/dungeon_crawler_fx/App.java
+++ src/main/java/com/krieger/dungeon_crawler_fx/App.java
@@ -6,6 +6,7 @@ import com.krieger.dungeon_crawler_fx.factories.ImageFactory;
 import com.krieger.dungeon_crawler_fx.factories.PaneFactory;
 import com.krieger.dungeon_crawler_fx.factories.SceneFactory;
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -31,30 +32,37 @@ public class App extends Application {
  */
 
         //NOTE Handlers
-    private void handleInventoryButton(ImageFactory ib, PaneFactory pf, ImageView iv) {
+                // handler methods are created here for now, will move them elsewhere too later in the process
+        private void handleInventoryButton(ImageFactory ib, PaneFactory pf, ImageView iv, List<Button> mainButtons, List<Button> inventoryButtons) {
+            ib.updateImage("file:./images/inventory_bg.jpg");
 
-        ib.updateImage("file:./images/inventory_bg.jpg");
+            updatePaneContent(pf, iv, inventoryButtons);
+            setButtonEventHandlers(inventoryButtons, ib, pf, iv, mainButtons, inventoryButtons);
+        }
 
-        List<Button> inventoryButtons = ButtonFactory.createNewButtons(MainController.inventoryButtons);
-
-        updatePaneContent(pf, iv, inventoryButtons);
-    }
-
-    private void handleBackButton(ImageFactory ib, PaneFactory pf, ImageView iv,List<Button>btnList) {
+    private void handleBackButton(ImageFactory ib, PaneFactory pf, ImageView iv, List<Button> mainButtons, List<Button> inventoryButtons) {
         ib.updateImage("file:./images/start_scene.jpg");
-
-        updatePaneContent(pf, iv,btnList);
+        updatePaneContent(pf, iv, mainButtons);
+        setButtonEventHandlers(mainButtons, ib, pf, iv, mainButtons, inventoryButtons);
     }
 
-    private void handleSearchButton(PaneFactory pf) {
-        Label messageOutput = new Label("Test Text!!!!");
+    public void clearOutput(PaneFactory pf){
+            for (Node l:pf.root.getChildren()){
+                if (l.getClass().equals(Label.class)){
+
+                    pf.root.getChildren().remove(l);
+                } else{continue;}
+            }
+    }
+    private void handleSearchButton(PaneFactory pf, String btnId) {
+        Label messageOutput = new Label(" ");
         messageOutput.setFont(new Font(28));
         messageOutput.setTextFill(PaneFactory.colorCode.get(4));
         messageOutput.setLayoutY(-200);
         messageOutput.setLayoutX(0);
         messageOutput.setTranslateX(0);
         messageOutput.setTranslateY(920);
-        messageOutput.setText(String.valueOf(messageOutput.getAlignment() + " " + " " + messageOutput.getLayoutX() + " " + messageOutput.getLayoutY()));
+        messageOutput.setText(String.valueOf(btnId+" " +messageOutput.getAlignment() + " " + " " + messageOutput.getLayoutX() + " " + messageOutput.getLayoutY()));
         pf.root.getChildren().add(messageOutput);
     }
     private void updatePaneContent(PaneFactory pf, ImageView iv, List<Button> btnList) {
@@ -66,6 +74,75 @@ public class App extends Application {
         pf.root.getChildren().add(pf.vBox);
     }
 
+            // new button-handler test, previous version was not successful
+    private void setButtonEventHandlers(List<Button> buttons, ImageFactory ib, PaneFactory pf, ImageView iv, List<Button> mainButtons, List<Button> inventoryButtons) {
+        for (Button btn : buttons) {
+            switch (btn.getId()){
+                case "inventoryBtn": {
+                    btn.setOnAction(a->{
+                        System.out.println("\t - inventory-button works!");
+
+                        handleInventoryButton(ib,pf,iv,mainButtons,inventoryButtons);
+                    });
+                }break;
+                case "actionBtn": {
+                    btn.setOnAction(a->{
+                        System.out.println("\t - action-button works!");
+
+
+                        clearOutput(pf);
+                        handleSearchButton(pf,btn.getId());
+
+                    });
+                }break;
+                case "searchBtn": {
+                    btn.setOnAction(a->{
+                        System.out.println("\t - search-button works!");
+
+
+                            // just for testing, will get an own method soon :)
+                        clearOutput(pf);
+                        handleSearchButton(pf,btn.getId());
+
+                    });
+                }break;
+                case "backBtn": {
+                    btn.setOnAction(a->{
+                        System.out.println("\t - back-button works!");
+
+                        handleBackButton(ib, pf, iv, mainButtons, inventoryButtons);
+                    });
+                }break;
+                case "useBtn": {
+                    btn.setOnAction(a->{
+                        System.out.println("\t - use-button works!");
+
+                        handleSearchButton(pf,btn.getId());
+                    });
+                }break;
+                case "equipBtn": {
+                    btn.setOnAction(a->{
+                        System.out.println("\t - equip-button works!");
+
+                    });
+                }break;
+                case "combineBtn": {
+                    btn.setOnAction(a->{
+                        System.out.println("\t - combine-button works!");
+
+
+                    });
+                }break;
+                default:
+                    try {
+                        throw new IllegalStateException("Unexpected value exception at:  " + btn.getId());
+                    } catch (IllegalStateException e) {
+                        throw new RuntimeException(e);
+                    }
+            }
+        }
+    }
+
 
         //NOTE start()-method
     @Override
@@ -74,84 +151,49 @@ public class App extends Application {
 
         try{
                         // image-stuff happens here for now
-            ImageFactory ib = new ImageFactory();
-            ib.setImgPath("file:./images/start_scene.jpg");
-            ImageView iv = ib.imageViewBuilder("imageView");
+            ImageFactory iF = new ImageFactory();
+            iF.setImgPath("file:./images/start_scene.jpg");
+            ImageView iV = iF.imageViewBuilder("imageView");
 
 
                         // create PaneFactory instance and the panes
-            PaneFactory pf = new PaneFactory();
-            pf.newPane("btnBox");
-            pf.newPane("mainContainer");
-            pf.newPane("root");
+            PaneFactory pF = new PaneFactory();
+            pF.newPane("btnBox");
+            pF.newPane("mainContainer");
+            pF.newPane("root");
 
                         // create buttons, local-btnList and assign them to the btnBox
 
 
-            List<Button> mainbuttons = ButtonFactory.createNewButtons(MainController.mainSceneButtons);
+            List<Button> mainButtons = ButtonFactory.createNewButtons(MainController.mainSceneButtons);
 
-            List<Button> currentButtons;
-            currentButtons = mainbuttons;
-
-            updatePaneContent(pf,iv,mainbuttons);
-
-                        // inventory-buttons. might move that elsewhere later, just to see if it works with my action handler in the lower section
+                    // inventory-buttons. might move that elsewhere later, just to see if it works with my action handler in the lower section
             List<Button> inventoryButtons = ButtonFactory.createNewButtons(MainController.inventoryButtons);
+
+
+            updatePaneContent(pF,iV,mainButtons);
+
+
+
 
 
                     //CHECK will need to move the scene related code in a later update to a separate class
 
-            Scene currentScene = new Scene(pf.root,PaneFactory.colorCode.get(3));
+            Scene currentScene = new Scene(pF.root,PaneFactory.colorCode.get(3));
 
                     //NOTE trying out a new approach on all the stage-stuff here
 
             SceneFactory sf = new SceneFactory();
             sf.setStageStuff(stage);
 
-            stage.setTitle("Dungeon_Adventure_V0.1.4");
+            stage.setTitle("Dungeon_Adventure_V0.1.5");
             stage.setScene(currentScene);
             stage.show();
 
 
                     //CHECK ButtonList too short.... check what's up with that later
 
-            for (Button btn : currentButtons) {
-                switch (btn.getId()){
-                    case "inventoryBtn": {
-                        btn.setOnAction(a->{
-                            System.out.println("\t - inventory-button works!");
-
-                            handleInventoryButton(ib,pf,iv);
-                        });
-                    }break;
-                    case "actionBtn": {
-                        btn.setOnAction(a->{
-                            System.out.println("\t - action-button works!");
-
-                        });
-                    }break;
-                    case "searchBtn": {
-                        btn.setOnAction(a->{
-                            System.out.println("\t - search-button works!");
-
-                            handleSearchButton(pf);
-                        });
-                    }break;
-                    case "backBtn": {
-                        btn.setOnAction(a->{
-                            System.out.println("\t - back-button works!");
-
-                            handleBackButton(ib,pf,iv,mainbuttons);
-                        });
-                    }break;
-                    default:
-                        throw new IllegalStateException("Unexpected value exception maybe? " + btn.getId()
-                    );
-
-
-                }
-            }
-
+           setButtonEventHandlers(mainButtons,iF,pF, iV,mainButtons, inventoryButtons);
 
         } catch(Exception e){
                     // Exception not thrown for now
