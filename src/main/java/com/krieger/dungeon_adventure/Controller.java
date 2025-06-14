8f673937c08028e601f8274a6e3708695f303466
Mat <matze.krieger@googlemail.com>
2025-06-14 23:57:15 +0200

    v0.1

--- src/main/java/com/krieger/dungeon_adventure/Controller.java
+++ src/main/java/com/krieger/dungeon_adventure/Controller.java
@@ -1,4 +1,35 @@
 package com.krieger.dungeon_adventure;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 public class Controller {
+
+        // start-view.fxml
+    @FXML
+    private HBox button_container;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private ImageView image_container;
+
+    @FXML
+    private Button loadGameButton;
+
+    @FXML
+    private VBox main_container;
+
+    @FXML
+    private Button newGameButton;
+
+    @FXML
+    private Button optionsButton;
+
+
+
 }
