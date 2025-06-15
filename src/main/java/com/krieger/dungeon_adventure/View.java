cd3291dce597d242c79b85f6ce196f7d1cf1910f
Mat <matze.krieger@googlemail.com>
2025-06-15 18:04:40 +0200

    v0.1

--- src/main/java/com/krieger/dungeon_adventure/View.java
+++ src/main/java/com/krieger/dungeon_adventure/View.java
@@ -5,32 +5,49 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class View {
     // This class is responsible for managing the user interface and switching scenes.
     // It will contain methods to load FXML files, set up the stage, and handle scene transitions.
 
-    public FXMLLoader loadInitialScene() {
+    private FXMLLoader loader;
+    private Stage newStage;
+    private Scene scene;
+
+    private String fxmlPath = "/com/krieger/dungeon_adventure/start-view.fxml";
+
+    public View() {
+
+        loadView();
+    }
+
+    private void loadView() {
         // Load the initial scene (e.g., start-view.fxml)
-        // This method will be implemented to set up the initial user interface.
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("com/krieger/dungeon_adventure/images/start_screen_bg_neu" +
-                ".jpg"));
-        return loader;
-    }
-    public void show(){
 
-        Stage newStage = new Stage();
-        newStage.setTitle("Dungeon Adventure");
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/krieger/dungeon_adventure/start-view.fxml"));
-            Scene scene = new Scene(loader.load());
-            newStage.setScene(scene);
-            newStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        this.loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            //this.loader.load();
+
+
     }
 
-    public void switchScene(String fxmlFile) {
-        // This method will handle switching to a different scene based on the provided FXML file.
+    public void show()throws IOException{
+        this.newStage = new Stage();
+        this.newStage.setTitle("Dungeon Adventure");
+        FXMLLoader loader = getLoader();
+        this.scene = new Scene(loader.load());
+        this.newStage.setScene(scene);
+        this.newStage.show();
+    }
+
+    public FXMLLoader getLoader(){
+
+        if(this.loader!=null) {
+            return this.loader;
+        } else if(this.loader==null){
+            loadView();
+            return this.loader;
+        };
+        return null;
     }
 }
